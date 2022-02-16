4fb3c0834f936e902289bea5f29040188eabc9e9
Viktor B <vbjorken@kth.se>
2022-02-16 15:56:30 +0100

    sieves fix osv

--- labb2/files-lab2/sieves.c
+++ labb2/files-lab2/sieves.c
@@ -6,6 +6,8 @@
 
 #define COLUMNS 6
 
+int column_count = 0;
+
 void print_number(int number){
 	printf("%10d", number);
 	column_count++;
@@ -14,9 +16,30 @@ void print_number(int number){
 	}
 }
 
-void print_sieves(int input){
-//här ska det skrivas kod
-//här med
+void print_sieves(int input_value){
+	int array_size = input_value - 1; // initterar arrayn vi kommer använda
+	char amount_numbers[array_size]; // lokal array
+
+	for(int i = 0; i < array_size; i++){
+	amount_numbers[i] = 1;
+	}
+
+
+	for(int i = 2; i <= (int) sqrt(input_value); i++){
+		if(amount_numbers[i] == 1){	//detta är för att kolla om vi redan kollat nummret
+			for(int j = i*i; j <= input_value; j+=i){ //för varje mulptipel av numret
+			amount_numbers[j] = 0; //makrerar som ett icke prime
+			}
+		}
+		
+	}
+	
+	//printa ut alla sanna värden från arrayn med start från plats 2	
+	for(int i = 2; i<= array_size; i++){
+		if(amount_numbers[i] == 1){
+		print_number(i);
+		}
+	}
 }
 
 
@@ -28,7 +51,7 @@ void print_sieves(int input){
 // char pointer points to a null-terminated string.
 int main(int argc, char *argv[]){
   if(argc == 2)
-    print_primes(atoi(argv[1]));
+    print_sieves(atoi(argv[1]));
   else
     printf("Please state an interger number.\n");
   return 0;
